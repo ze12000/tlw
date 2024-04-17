4815358e5628ddf8d98e99585355b880b859c59a
ze12000 <167222274+ze12000@users.noreply.github.com>
2024-04-17 16:06:00 +0200

    Add files via upload

---
+++ OWNER.py
@@ -0,0 +1,10 @@
+# متطلبات التنصيب #
+OWNER = ["L_HLN","TR_E2S_ON_MY_MOoN"]
+OWNER_NAME = "𝐎ꪶ𝐋ٓ𝐄𝐕ِ𝐑┊͙🚸ݪـي٘فـࢪꪆُﺎ"
+BOT_TOKEN = "5552982639:AAEcoHhXUFO_3fmyxpEUPLHO8mCZ0ZeD63o"
+DATABASE = "mongodb+srv://Elkber:Elkber@cluster0.feuljpn.mongodb.net/?retryWrites=true&w=majority"
+CHANNEL = "https://t.me/sourceav"
+GROUP = "https://t.me/G_D_J1"
+VIDEO = "https://telegra.ph/file/2514530559cc173845e3f.jpg"
+LOGS = "G_D_J1"
+
